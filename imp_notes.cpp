fd8f791c955e85c9ebd97faee08c284253ffc115
Chang <chang2394@gmail.com>
2014-01-31 00:26:56 +0530

    Update imp_notes.cpp

--- imp_notes.cpp
+++ imp_notes.cpp
@@ -1,5 +1,5 @@
 
-///   CONVERSIONS :- 
+///   CONVERSIONS  ..........
 
 Conversion from DOUBLE TO INT :-
 double f;
@@ -17,13 +17,12 @@ Here divided by 100
 Code :- http://ideone.com/GFaK0u 
 
 
-
-///   SCANF WITH NEWLINE
-
+///   SCANF WITH NEWLINE .......
 scanf("%[^\n]%*c",str);
 
 
-///   FAST INPUT AND OUTPUT
+///   FAST INPUT AND OUTPUT ....
+
 int scan_d()    {int ip=getchar_unlocked(),ret=0,flag=1;for(;ip<'0'||ip>'9';ip=getchar_unlocked())if(ip=='-'){flag=-1;ip=getchar_unlocked();break;}for(;ip>='0'&&ip<='9';ip=getchar_unlocked())ret=ret*10+ip-'0';return flag*ret;}
 
 void print_d(int n)     {if(n<0){n=-n;putchar_unlocked('-');}int i=10;char output_buffer[10];do{output_buffer[--i]=(n%10)+'0';n/=10;}while(n);do{putchar_unlocked(output_buffer[i]);}while(++i<10);}
