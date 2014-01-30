4cd72816922374cadec9ece0b52da452c2a8ed1d
Chang <chang2394@gmail.com>
2014-01-30 18:50:44 +0530

    Create fast_input.cpp

---
+++ fast_input.cpp
@@ -0,0 +1,12 @@
+
+inline void fi(int *a)
+{
+    register char c = 0;
+    while (c < 33)
+	c = getchar_unlocked();
+    *a = 0;
+    while (c > 33) {
+	*a = *a * 10 + c - '0';
+	c = getchar_unlocked();
+    }
+}
