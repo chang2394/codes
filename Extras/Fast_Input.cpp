2d6f0cb491562443d6d1d65cb4e009c88cf97164
Chang <chang2394@gmail.com>
2014-08-06 08:50:32 +0530

    Update Fast_Input.cpp

--- Extras/Fast_Input.cpp
+++ Extras/Fast_Input.cpp
@@ -2,15 +2,15 @@
 template <typename T>
 inline void fi(T *a)
 {
- register char c=0;
- while (c<33) c=getchar_unlocked();
- *a=0;
- int tmp = 0;
- while (c>33)
- {
-     if ( c == 45 ) tmp = 1;
-     else *a=*a*10+c-'0';
-     c=getchar_unlocked();
- }
- if ( tmp == 1 ) *a = 0-(*a);
+    register char c=0;
+    while (c<33) c=getchar_unlocked();
+    *a=0;
+    int tmp = 0;
+    while (c>33)
+    {
+        if ( c == 45 ) tmp = 1;
+        else *a=*a*10+c-'0';
+        c=getchar_unlocked();
+    }
+    if ( tmp == 1 ) *a = 0-(*a);
 }
